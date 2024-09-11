2c87aab1b9eb9d0f639e78f39c1fab49a74d32f4
ANDANI GOWDA M R <118588329+Andanigowda2903@users.noreply.github.com>
2024-09-11 14:48:14 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"; // Import Link from React Router DOM
 import AppBar from "../components/AppBar";
-import logo from "../assests/Logo.svg";
+import google from "../assets/google.svg";
 import Footer from "../components/Footer";
 
 function Login() {
